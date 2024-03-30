30ddc9a22c9a1592fbb49b1d95d63d30d47c87c3
Monthop Champaruang <monthop@sumana.org>
2024-03-30 06:24:12 +0000

    test transformers and torch

---
+++ devel/test-transformers-torch/train.py
@@ -0,0 +1,42 @@
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling
+from transformers import Trainer, TrainingArguments
+
+# Load tokenizer and model
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+# Prepare dataset
+train_dataset = TextDataset(
+    tokenizer=tokenizer,
+    file_path="./train_dataset.txt",
+    block_size=128
+)
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer, mlm=False
+)
+
+# Training arguments
+training_args = TrainingArguments(
+    output_dir="./output",
+    overwrite_output_dir=True,
+    num_train_epochs=3,
+    per_device_train_batch_size=16,
+    save_steps=10_000,
+    save_total_limit=2,
+)
+
+# Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    data_collator=data_collator,
+    train_dataset=train_dataset,
+)
+
+# Train the model
+trainer.train()
+
+# Save model
+model.save_pretrained("./my_gpt2_model")
+tokenizer.save_pretrained("./my_gpt2_tokenizer")
