c2e44733ebadb40de7b2a076fd3c70c7c54eb55f
Monthop Champaruang <monthop@sumana.org>
2024-03-30 16:19:46 +0000

    add datasets

--- 1-go-devel.sh
+++ 1-go-devel.sh
@@ -2,4 +2,6 @@ sudo mkdir /tmp/pn
 sudo chown codespace:codespace /tmp/pn
 sudo mkdir /tmp/transformers
 sudo chown codespace:codespace /tmp/transformers
-docker run --rm -it --user 1000 -v /tmp/pn:/home/pn -v /tmp/transformers:/opt/transformers -v /workspaces/aichat-codespace-docker/devel:/opt/devel nikolaik/python-nodejs:python3.10-nodejs18-bullseye bash
+sudo mkdir /tmp/datasets
+sudo chown codespace:codespace /tmp/datasets
+docker run --rm -it --user 1000 -v /tmp/pn:/home/pn -v /tmp/datasets:/opt/datasets -v /tmp/transformers:/opt/transformers -v /workspaces/aichat-codespace-docker/devel:/opt/devel nikolaik/python-nodejs:python3.10-nodejs18-bullseye bash
